56780077cc1de4b45070392dde44a933d7aa75c1
Andriy Lendyel <flame27@meta.ua>
2015-07-06 16:52:26 +0300

    AL: committing apk

--- src/com/softserveinc/furniture/FurnitureApplication.java
+++ src/com/softserveinc/furniture/FurnitureApplication.java
@@ -17,7 +17,7 @@ public class FurnitureApplication extends Application {
 					new FurnitureListItem(R.drawable.air_conditioning, "Air conditioning", "internal-air-conditioning.obj"),
 					new FurnitureListItem(R.drawable.lavatory, "Lavatory", "Lavatory.obj"),
 					new FurnitureListItem(R.drawable.statue, "Statue", "Statue.obj"),
-					new FurnitureListItem(R.drawable.basketball, "Basketball", "Basketball.blend")));
+					new FurnitureListItem(R.drawable.basketball, "Basketball", "Basketball.obj")));
 				
 	public List<FurnitureListItem> getItemsList() {
 		return items;
